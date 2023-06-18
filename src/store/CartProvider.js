d94320c97b5216c9eba9d8f7ef888e9728a4de56
ali mert inal <mert.inal@gmail.com>
2023-06-18 12:29:27 +0200

    add item index

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,8 +9,13 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    const updatedItems = state.items.concat(action.item);
+ 
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
